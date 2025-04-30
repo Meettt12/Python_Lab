88013c41ae6407d0b8e2ee1cde41c519e315af5d
Meet Patel <meet.p1212@icloud.com>
2025-04-30 21:34:38 +0530

    Update Prog4.py

--- Lab_10/Prog4.py
+++ Lab_10/Prog4.py
@@ -1,7 +1,8 @@
-#Create a specific subdirectory and copy
-#one file from another subdirectory to this newly created subdirectory.
-f = open(r"C:\Users\rahul\OneDrive\Desktop\Book1.csv")
-f1 = open(r"C:\Users\rahul\OneDrive\Desktop\Book2.csv","w+")
+"""Create a specific subdirectory and copy one file from another subdirectory to this newly created subdirectory."""
+
+
+f = open(r"D:\College\Python\Lab10\Excel1.csv")
+f1 = open(r"D:\College\Python\Lab10\Excel2.csv","w+")
 for line in f:
     print(line)
     f1.write(line)
