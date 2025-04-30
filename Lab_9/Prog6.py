4a1397c989a9d0f0ee617754a675d4760e89a2fa
Meet Patel <meet.p1212@icloud.com>
2025-04-30 20:23:42 +0530

    Update and rename 9.7.py to Prog6.py

--- Lab_9/Prog6.py
+++ Lab_9/Prog6.py
@@ -1,12 +1,12 @@
+#    Function
+
 def ispalidrom(str):
     reversed_s = "".join(reversed(str)).lower()
     s = ''.join(str).lower()
     cleaned_revers_s = "".join(char for char in reversed_s if char.isalnum())
     cleaned_s = "".join(char for char in s if char.isalnum())
 
-    #s = "hello"
- #reversed_s = s[::-1]
-#print(reversed_s)  # Output: "olleh"
+
     if cleaned_s== cleaned_revers_s:
         print('The string is palidrom')
     else :
