a0ec552bf693af24bfa668f4d2656df817a504e0
Meet Patel <meet.p1212@icloud.com>
2025-04-30 19:50:11 +0530

    Update 6.2.py

--- Lab_6/6.2.py
+++ Lab_6/6.2.py
@@ -1,6 +1,9 @@
-def q2():
+#Tuple
+
+def prob2():
         l=[(100,'Ronit',19),(99,'Yug',18),(120,'Krish',18),(159,'Tanmesh',18)]
         roll=[i[0] for i in l]
         name=[i[1] for i in l]
         age=[i[2] for i in l]
         print(roll,name,age)
+prob2()
