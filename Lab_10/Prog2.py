683932688a098c4511fce28bb9b9a96fc24ad7f0
Meet Patel <meet.p1212@icloud.com>
2025-04-30 21:30:25 +0530

    Update Prog2.py

--- Lab_10/Prog2.py
+++ Lab_10/Prog2.py
@@ -1,9 +1,11 @@
-f= open("Book1.csv")
+# Files
+
+f= open("Excel1.csv")
 empty_dictionary = {}
 data = f.readlines()
-print('The readlines is',data,'\n')
+print('The Readlines Is',data,'\n')
 filter = [lines.strip().split(',') for lines in data]
-print('The fileter is ',filter,'\n')
+print('The Fileter Is ',filter,'\n')
 for i in range(len(data)):
     empty_dictionary[filter[0][i]] = [x[i] for x in filter[1:]]
     
