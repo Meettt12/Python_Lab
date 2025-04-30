6b10ac5d900514db7091bd33d7408662df8da6e6
Meet Patel <meet.p1212@icloud.com>
2025-04-30 19:43:41 +0530

    Update and rename 5.5.py to Prob5.py

--- Lab_5/Prob5.py
+++ Lab_5/Prob5.py
@@ -1,5 +1,10 @@
-def q5():
+#List
+
+def List():
     strings = ["hello", "world", "python", "code"]
     for i in range(len(strings)):
         strings[i] = strings[i].upper()
     print(strings)
+
+
+List()
