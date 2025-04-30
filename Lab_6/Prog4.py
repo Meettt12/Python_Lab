9d4529e691fd4af8afa4f9fd602b74bfe3b9f710
Meet Patel <meet.p1212@icloud.com>
2025-04-30 19:56:13 +0530

    Update and rename 6.4.py to Prog4.py

--- Lab_6/Prog4.py
+++ Lab_6/Prog4.py
@@ -1,8 +1,12 @@
+#Tuble Mrthod
+
 import operator
 def q4():
     l=[('Alloo',100),('Burger',120),('pizza',200)]
     lst=sorted(l,key=operator.itemgetter(1),reverse=True)
     print(lst)
+
+q4()
     
     
     
