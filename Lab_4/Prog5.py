d7e0ef7e531e05a52f3131b3d3c947c0e33e9de7
Meet Patel <meet.p1212@icloud.com>
2025-04-30 19:38:37 +0530

    Update and rename pythagoras.py to Prog5.py

--- Lab_4/Prog5.py
+++ Lab_4/Prog5.py
@@ -1,7 +1,9 @@
-def q5():
+#Pythagoras
+
+def py():
     for a in range(1, 31):
         for b in range(a, 31):
             for c in range(b, 31):
                 if a * a + b * b == c * c:
                     print(a, b, c)
-q5()
+py()
