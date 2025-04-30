b3d0cbae8f5364359161fdefb95fda65e0c40b5f
Meet Patel <meet.p1212@icloud.com>
2025-04-30 21:36:05 +0530

    Update Prog5.py

--- Lab_10/Prog5.py
+++ Lab_10/Prog5.py
@@ -1,7 +1,8 @@
-#Write a program to copy contents of one file to another.
-#While doing so, replace all lowercase characters into uppercase characters.
-f = open(r"C:\Users\rahul\OneDrive\Desktop\Book1.csv")
-f1 = open(r"C:\Users\rahul\OneDrive\Desktop\Book3.csv","w+")
+"""Write a program to copy contents of one file to another. While doing so, replace all lowercase characters into uppercase characters."""
+
+
+f = open(r"D:\College\Python\Lab10\Excel1.csv")
+f1 = open(r"D:\College\Python\Lab10\Excel3.csv","w+")
 for line in f:
     print(line)
     f1.write(line.upper())
