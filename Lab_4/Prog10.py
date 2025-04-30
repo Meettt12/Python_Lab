bad527737159c802828b0c4709bf08f158aa6f2e
Meet Patel <meet.p1212@icloud.com>
2025-04-30 19:36:20 +0530

    Update and rename fibonacci.py to Prog10.py

--- Lab_4/Prog10.py
+++ Lab_4/Prog10.py
@@ -1,6 +1,10 @@
-def q10():
+#Fibonaci Series
+
+def Series():
     n = int(input("Enter the value of n: "))
     a, b = 0, 1
     for _ in range(n):
         print(a)
         a, b = b, a + b
+
+Series()
